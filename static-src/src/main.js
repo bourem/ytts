f33c7b612e5bd0b295f8faa39b9b818638091e21
RB <bouchezr@avep.io>
2017-03-12 22:12:45 +0900

    Extracted store from main.js

--- static-src/src/main.js
+++ static-src/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
-Vue.use(Vuex);
+import { mapState } from 'vuex';
+import store from './store';
 
 import TwoStepsSelection from './components/TwoStepsSelection.vue';
 import SubtitlesList from './components/SubtitlesList.vue';
@@ -10,95 +10,6 @@ window.ytts = {};
 
     Vue.prototype.$http = axios;
     
-    const store = new Vuex.Store({
-        state: {
-            subtitles: view_init.subtitles,
-            video: view_init.video,
-            version: view_init.version,
-            availableVersions: view_init.versions,
-            csrftoken: getCookie('csrftoken'),
-            isSaving: false,
-            lastSaveTime: null
-        },
-        mutations: {
-            setSubtitles (state, payload) {
-                if (payload.newSubtitles) state.subtitles = payload.newSubtitles;
-                if (payload.newVersion) state.version = payload.newVersion;
-                if (payload.newVideo) state.video = payload.newVideo;
-            },
-            addSubtitle (state, payload) {
-                let newSubtitle = {};
-                if (payload) {
-                    newSubtitle = {
-                        subtitle: payload.subtitle,
-                        start: payload.start,
-                        stop: payload.stop,
-                    };
-                }
-                state.subtitles.push(newSubtitle);
-            },
-            savingStarted (state) {
-                state.isSaving = true;
-            },
-            savingStopped (state) {
-                state.isSaving = false;
-            },
-            updateLastSave () {
-                state.lastSaveTime = Date.now();
-            }
-        },
-        actions: {
-            loadSubtitles ({ state, commit }, versionName) {
-                axios.get("/ytts/" + state.video + "/load/", {
-                    params: {
-                        version_name: versionName,
-                    },
-                    responseType: "json"
-                }).then(function (response) {
-                    let payload = {
-                        newSubtitles: response.data,
-                        newVersion: versionName,
-                    };
-                    commit('setSubtitles', payload);
-                });
-            },
-            saveSubtitles: function ({ state, commit }) {
-                return new Promise((resolve, reject) => {
-                    commit('savingStarted');
-                    let url = "/ytts/" + state.video + "/save/";
-                    axios.post(url,
-                        'subtitles_json=' + JSON.stringify(state.subtitles) + '&version_name=' + state.version,
-                        {
-                            headers: {
-                                'Content-type': "application/x-www-form-urlencoded",
-                                'X-CSRFToken': state.csrftoken
-                            }
-                        })
-                        .then(function (response) {
-                            commit('savingStopped');
-                            if (response.status == "OK") {
-                                commit('updateLastSave');
-                            }
-                            console.log(response);
-                            resolve();
-                        });
-                });
-            },
-            createSubtitles: function ({ state, commit, dispatch }, payload) {
-                let newSubtitlesPayload = {
-                    newVideo: state.video,
-                    newVersion: payload.version
-                };
-                if (!payload.isClone) {
-                    newSubtitlesPayload.newSubtitles = [];
-                }
-                commit('setSubtitles', newSubtitlesPayload);
-                dispatch('saveSubtitles');
-            }
-        }
-    });
-
-
     window.vYttsApp = new Vue({
         components: {
             "ytts-twostepsselection": TwoStepsSelection,
@@ -107,7 +18,7 @@ window.ytts = {};
         },
         el: "#yttsApp",
         store,
-        computed: Vuex.mapState({
+        computed: mapState({
             loadedVersion: 'version',
             loadedVideo: 'video',
             availableVersions: 'availableVersions',
@@ -295,19 +206,4 @@ window.ytts = {};
         return activeLines;
     };
 
-    function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie !== '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = cookies[i].trim();
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
 
---
+++ static-src/src/store/index.js
@@ -0,0 +1,93 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
+import { getCookie } from '../utils/utils.js';
+
+export default new Vuex.Store({
+    state: {
+        subtitles: view_init.subtitles,
+        video: view_init.video,
+        version: view_init.version,
+        availableVersions: view_init.versions,
+        csrftoken: getCookie('csrftoken'),
+        isSaving: false,
+        lastSaveTime: null
+    },
+    mutations: {
+        setSubtitles (state, payload) {
+            if (payload.newSubtitles) state.subtitles = payload.newSubtitles;
+            if (payload.newVersion) state.version = payload.newVersion;
+            if (payload.newVideo) state.video = payload.newVideo;
+        },
+        addSubtitle (state, payload) {
+            let newSubtitle = {};
+            if (payload) {
+                newSubtitle = {
+                    subtitle: payload.subtitle,
+                    start: payload.start,
+                    stop: payload.stop,
+                };
+            }
+            state.subtitles.push(newSubtitle);
+        },
+        savingStarted (state) {
+            state.isSaving = true;
+        },
+        savingStopped (state) {
+            state.isSaving = false;
+        },
+        updateLastSave () {
+            state.lastSaveTime = Date.now();
+        }
+    },
+    actions: {
+        loadSubtitles ({ state, commit }, versionName) {
+            axios.get("/ytts/" + state.video + "/load/", {
+                params: {
+                    version_name: versionName,
+                },
+                responseType: "json"
+            }).then(function (response) {
+                let payload = {
+                    newSubtitles: response.data,
+                    newVersion: versionName,
+                };
+                commit('setSubtitles', payload);
+            });
+        },
+        saveSubtitles: function ({ state, commit }) {
+            return new Promise((resolve, reject) => {
+                commit('savingStarted');
+                let url = "/ytts/" + state.video + "/save/";
+                axios.post(url,
+                    'subtitles_json=' + JSON.stringify(state.subtitles) + '&version_name=' + state.version,
+                    {
+                        headers: {
+                            'Content-type': "application/x-www-form-urlencoded",
+                            'X-CSRFToken': state.csrftoken
+                        }
+                    })
+                    .then(function (response) {
+                        commit('savingStopped');
+                        if (response.status == "OK") {
+                            commit('updateLastSave');
+                        }
+                        console.log(response);
+                        resolve();
+                    });
+            });
+        },
+        createSubtitles: function ({ state, commit, dispatch }, payload) {
+            let newSubtitlesPayload = {
+                newVideo: state.video,
+                newVersion: payload.version
+            };
+            if (!payload.isClone) {
+                newSubtitlesPayload.newSubtitles = [];
+            }
+            commit('setSubtitles', newSubtitlesPayload);
+            dispatch('saveSubtitles');
+        }
+    }
+});
